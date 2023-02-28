b90c012a406aaf359cf65469bc4e46aa0bc958bb
isaac-crossing <117688143+isaac-crossing@users.noreply.github.com>
2023-02-28 05:43:38 -0800

    Study nextjs doc

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,12 @@
+import { loadPosts } from '../lib/load-posts'
+
 export default function Blog({ posts }) {
   return (
     <ul>
-      {/* {posts.map((post) => (
-        <li>{post.title}</li>
-      ))} */}
-      { posts.storeId }
+      {posts.map((post) => (
+        // eslint-disable-next-line react/jsx-ke
+        <li>{ post.name }</li>
+      ))}
     </ul>
   )
 }
@@ -12,8 +14,8 @@ export default function Blog({ posts }) {
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
-  const res = await fetch('https://www.mocky.io/v3/a62fa770-9bff-4839-be5c-a203317898e0')
-  const posts = await res.json()
+  // const res = await fetch('https://www.mocky.io/v3/a62fa770-9bff-4839-be5c-a203317898e0')
+  const posts = await loadPosts();
 
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,6 +8,16 @@ export default function Post({ post }) {
 
 // 빌드 시에 static하게 생성할 페이지를 정함
 export async function getStaticPaths() {
+  // When this is true (in preview environments) don't
+  // prerender any static pages
+  // (faster builds, but slower initial page load)
+  if (process.env.SKIP_BUILD_STATIC_GENERATION) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    }
+  }
+  
   // Call an external API endpoint to get posts
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const posts = await res.json();
@@ -15,7 +25,7 @@ export async function getStaticPaths() {
     params: { id: post.id.toString() },
   }));
 
-  return { paths, fallback: true };
+  return { paths, fallback: false };
 }
 
 // 빌드 시 데이터를 fetch하여 static페이지를 생성
